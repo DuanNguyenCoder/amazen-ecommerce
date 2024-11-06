15c1e5038b7176a41015f4273a342eb72ac9e9e8
duannguyen242 <duannguyen242@gmail.com>
2024-11-06 21:30:21 +0700

    hash passwords user

--- server/models/user.js
+++ server/models/user.js
@@ -19,5 +19,33 @@ const UserSchema = new Schema({
   },
   created: { type: Date, default: Date.now },
 });
+//Function to handleEvent of password modification
+UserSchema.pre("save", function (next) {
+  var user = this;
 
+  if (!user.isModified("password")) return next();
+
+  bcrypt.hash(user.password, null, null, function (err, hash) {
+    if (err) return next(err);
+
+    user.password = hash;
+    next();
+  });
+});
+
+//Function to check if modified and saved passwords match
+UserSchema.methods.comparePassword = function (password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
+//Function to Use gravatar Service for image Sizes
+UserSchema.methods.gravatar = function (size) {
+  if (!this.size) size = 200;
+  if (!this.email) {
+    return "https://gravatar.com/avatar/?s" + size + "&d=retro";
+  } else {
+    var md5 = crypto.createHash("md5").update(this.email).digest("hex");
+    return "https://gravatar.com/avatar/" + md5 + "?s" + size + "&d=retro";
+  }
+};
 module.exports = mongoose.model("User", UserSchema);
