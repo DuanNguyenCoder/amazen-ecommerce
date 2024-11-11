542347d773d6b2f719c6ab931a747cfe8af31aa5
duannguyen242 <duannguyen242@gmail.com>
2024-11-11 21:30:21 +0700

    check JWT

---
+++ server/middlewares/check-jwt.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+module.exports = function (req, res, next) {
+  let token = req.headers["authorization"];
+
+  if (token) {
+    jwt.verify(token, process.env.secret, function (err, decoded) {
+      if (err) {
+        res.json({
+          success: false,
+          message: "Failed to authenticate token",
+        });
+      } else {
+        req.decoded = decoded;
+        next();
+      }
+    });
+  } else {
+    res.status(403).json({
+      success: false,
+      message: "No token provided",
+    });
+  }
+};
