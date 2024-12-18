bf9aebafcfadcc15f98b1033ffd52248b38deaa4
duannguyen242 <duannguyen242@gmail.com>
2024-12-18 21:30:21 +0700

    modify env

---
+++ client/Angularecommerce/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  stripeKey:
+    "pk_test_51QrFBYGHMlFXVQaaz2H1Z1Yc3c3B8doFHGeanJKPvvBF3kHBC6YfxB52GuE27ZHTAkd2DUjzW0qzN1vHaBFsGdU400eUVAwbfp",
+  apiURL: "http://localhost:3030/api/",
+};
